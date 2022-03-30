42fec8422eaa0d817bad900ad488ab428713fd1f
aniketmandloi <91334272+aniketmandloi@users.noreply.github.com>
2022-03-30 22:17:23 +0530

    Add files via upload

---
+++ Driving_eligibility.py
@@ -0,0 +1,9 @@
+print("Enter your Age ")
+age=int(input())
+
+if age > 18:
+    print("you can Drive")
+elif age < 18:
+    print("you cannot drive")
+else:
+    print("we are not able to decide, you may come and give the test")
